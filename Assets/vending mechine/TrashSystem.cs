df04dafd9d5a51a6ab482edcc5db82c654a0d768
vbdp <vbd@vbd.es>
2025-12-21 16:45:29 +0100

    fotos de latas agregadas

---
+++ Assets/vending mechine/TrashSystem.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TrashSystem : UdonSharpBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Pickup"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+}
