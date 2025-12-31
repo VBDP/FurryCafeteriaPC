820038d8a057055ee83f9d0ffc55cef631640de2
vbdp <vbd@vbd.es>
2025-12-31 12:36:45 +0100

    Creada puerta a zona de admins, desactivado efecto bajo el agua

---
+++ Assets/Keypad/Shaders/CustomPrograms/TeleportProgram.cs
@@ -0,0 +1,17 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TeleportProgram : UdonSharpBehaviour
+{
+    public BoxCollider door;
+       public void keypadGranted()
+    {
+        Debug.Log("Player Granted");
+        door.enabled = true;
+        //Networking.LocalPlayer.TeleportTo(transform.position, transform.rotation);
+    }
+}
+
---
+++ Assets/Keypad/Shaders/Keypad_UdonSharp/TeleportOnClick.cs
@@ -0,0 +1,21 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class TeleportOnClick : UdonSharpBehaviour
+{
+    [Header("Punto de destino")]
+    public Transform destino;
+
+    public override void Interact()
+    {
+        VRCPlayerApi player = Networking.LocalPlayer;
+
+        if (player == null || destino == null)
+            return;
+
+        // Teletransportar al jugador local
+        player.TeleportTo(destino.position, destino.rotation);
+    }
+}
