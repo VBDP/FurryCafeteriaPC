fa253660791ebeb698879aee0a219f2e5627aeb6
vbdp <vbd@vbd.es>
2026-06-11 10:00:12 +0200

    ELiminados elementos que solo servian para desoptimizar el mundo, cambiado logo, quitados grupos, agregado mi grupo

--- Assets/3. Scripts/DisableAudioSources.cs
+++ Assets/3. Scripts/DisableAudioSources.cs
@@ -6,8 +6,12 @@ using VRC.Udon;
 public class DisableAudioSources : UdonSharpBehaviour
 {
     public AudioSource targetAudio;
+    public AudioSource targetAudio2;
+    public AudioSource targetAudio3;
 
     private bool originalState;
+    private bool originalState2;
+    private bool originalState3;
 
     public override void OnPlayerTriggerEnter(VRCPlayerApi player)
     {
@@ -20,6 +24,18 @@ public class DisableAudioSources : UdonSharpBehaviour
 
             // Desactivar siempre al entrar
             targetAudio.enabled = false;
+        }
+
+        if (targetAudio2 != null)
+        {
+            originalState2 = targetAudio2.enabled;
+            targetAudio2.enabled = false;
+        }
+
+        if (targetAudio3 != null)
+        {
+            originalState3 = targetAudio3.enabled;
+            targetAudio3.enabled = false;
         }
     }
 
